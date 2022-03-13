2bf1ee804a86aa10fae6fdf694109c5020ce9c3d
Subu <59548115+Subu19@users.noreply.github.com>
2022-03-13 15:43:50 +0545

    idk what i did

---
+++ src/admin/app.js
@@ -0,0 +1,39 @@
+import AuthLogo from "./extensions/logo.svg";
+import MenuLogo from "./extensions/logo.svg";
+import favicon from "./extensions/logo.svg";
+export default {
+  config: {
+    // Replace the Strapi logo in auth (login) views
+    auth: {
+      logo: AuthLogo,
+    },
+    // Replace the favicon
+    head: {
+      favicon: favicon,
+    },
+    // Add a new locale, other than 'en'
+    locales: ["fr", "de"],
+    // Replace the Strapi logo in the main navigation
+    menu: {
+      logo: MenuLogo,
+    },
+    // Override or extend the theme
+    theme: {
+      colors: {
+        primary100: "#f6ecfc",
+        primary200: "#e0c1f4",
+        primary500: "#ac73e6",
+        primary600: "#9736e8",
+        primary700: "#8312d1",
+        danger700: "#b72b1a",
+      },
+    },
+    // Extend the translations
+    // Disable video tutorials
+    tutorials: false,
+    // Disable notifications about new Strapi releases
+    notifications: { release: false },
+  },
+
+  bootstrap() {},
+};
